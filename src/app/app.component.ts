e96a7c3d63c0a98006eb5247af635d4cd9ad5a5c
MRaj <pmrmuthuraj@gmail.com>
2019-07-13 18:18:22 +0530

    Electron and SQLlite integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from '../assets/model/item.schema';
+import { AppService } from './app.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'electron-sqllite-angular';
+  itemList: Item[];
+  constructor(private translate: TranslateService, private appservice: AppService) {
+    translate.setDefaultLang('en');
+  }
+
+  ngOnInit(): void {
+    console.log('component initialized');
+    this.appservice.getItems().subscribe((items) => (this.itemList = items));
+  }
+
+  addItem(): void {
+    let item = new Item();
+    item.name = 'Item ' + this.itemList.length;
+    this.appservice.addItem(item).subscribe((items) => (this.itemList = items));
+  }
+
+  deleteItem(): void {
+    const item = this.itemList[this.itemList.length - 1];
+    this.appservice
+      .deleteItem(item)
+      .subscribe((items) => (this.itemList = items));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ElectronService } from 'ngx-electron';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppService } from './app.service';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +21,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (HttpLoaderFactory),
+        deps: [HttpClient]
+      }
+    })
   ],
-  providers: [],
+  providers: [ElectronService, AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
